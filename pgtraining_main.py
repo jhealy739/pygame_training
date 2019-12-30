41886d5558f8668dffa8f43acf197fb47115b2e9
John Healy <jhealy739@gmail.com>
2019-12-29 20:21:14 -0600

    nothing really

--- pgtraining_main.py
+++ pgtraining_main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     #Initialize the game and create a screen object
@@ -11,6 +12,8 @@ def run_game():
     pygame.display.set_caption("Again!?!?")
     bg_color = (ai_settings.bg_color)
 
+    ship = Ship(screen)
+
     #Start the main loop for the game
     while True:
         #Watch for keyboard and mouse events
@@ -20,6 +23,7 @@ def run_game():
 
         #Redraw the screen during each pass through the loop.
         screen.fill(bg_color)
+        ship.blitme()
         #Make the most recently drawn screen visible
         pygame.display.flip()
 
