0bf73c04a5e518823e24a01bd92cc633a2177395
John Healy <jhealy739@gmail.com>
2019-12-29 20:34:48 -0600

    created game_functions to refactor main

--- pgtraining_main.py
+++ pgtraining_main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #Initialize the game and create a screen object
@@ -17,14 +18,8 @@ def run_game():
     #Start the main loop for the game
     while True:
         #Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        #Redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
-        ship.blitme()
-        #Make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings.bg_color, screen, ship)
+        
 
 run_game()
--- ship.py
+++ ship.py
@@ -10,6 +10,7 @@ class Ship():
         #Load the ship image and get its rect.
         self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
         #Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
