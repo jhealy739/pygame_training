cf160f424f2e74188600caa2469f71d555709f3f
John Healy <jhealy739@gmail.com>
2020-01-02 22:33:23 -0600

    ship now has variable speed

--- ship.py
+++ ship.py
@@ -15,6 +15,8 @@ class Ship():
         #Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.pos_x = self.rect.centerx
+        self.pos_y = self.rect.centery
 
         #Movement flags. When they are True the ship will move one px for screen flip
         self.moving_right = False
@@ -22,19 +24,29 @@ class Ship():
         self.moving_up = False
         self.moving_down = False
 
+        #Set the ship speed from Settings class
+        self.speed = .5
+
+        print (self.pos_x)
+
+
     def update(self):
         #Update the ships rect position based on movement flags
         if ((self.moving_right) & (self.rect.right < self.screen_rect.right)):
-            self.rect.centerx += 1
+            self.pos_x += self.speed
+            self.rect.centerx = self.pos_x
 
         if ((self.moving_left) & (self.rect.left > self.screen_rect.left)):
-            self.rect.centerx -= 1
+            self.pos_x -= self.speed
+            self.rect.centerx = self.pos_x
 
         if ((self.moving_up) & (self.rect.top > self.screen_rect.top)):
-            self.rect.bottom -= 1
+            self.pos_y -= self.speed
+            self.rect.centery = self.pos_y
 
         if ((self.moving_down) & (self.rect.bottom < self.screen_rect.bottom)):
-            self.rect.bottom += 1
+            self.pos_y += self.speed
+            self.rect.centery = self.pos_y
 
     def blitme(self):
         #Check movement flags and then draw the ship at its current location.
